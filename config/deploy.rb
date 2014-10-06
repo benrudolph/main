838285d5fea2350a582af1db03bcc918b040b3e5
Ben Rudolph <rudolphben@gmail.com>
2014-10-06 21:49:03 +0200

    New deploy process

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,11 +19,11 @@ set :ssh_options, { :forward_agent => true }
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :jekyll do
+  task :generate, :roles => :app do
+    run "cd #{release_path} && bundle exec jekyll build"
+  end
+end
+
+after 'deploy:update_code', 'deploy:cleanup'
+after 'bundle:install', 'jekyll:generate'
