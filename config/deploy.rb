7f8d74b6a2ed4440b67f9656215a12848d39c9d8
Ben Rudolph <rudolphben@gmail.com>
2014-10-06 21:54:46 +0200

    A few more deploy things

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,10 @@
 set :application, "benrudolph"
 set :repository,  "git@github.com:benrudolph/main.git"
 set :scm, :git
-set :deploy_via, :copy
+set :deploy_via, :remote_cache
 set :user, :deploy
 set :deploy_to, "/var/www/#{application}"
+set :use_sudo, false
 default_run_options[:pty] = true
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
@@ -20,7 +21,7 @@ set :ssh_options, { :forward_agent => true }
 # these http://github.com/rails/irs_process_scripts
 
 namespace :jekyll do
-  task :generate, :roles => :app do
+  task :generate, :roles => :blog do
     run "cd #{release_path} && bundle exec jekyll build"
   end
 end
