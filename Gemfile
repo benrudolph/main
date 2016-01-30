73e6a51b686a0967b833a7b3c28c648fb82078f4
Ben <rudolphben@gmail.com>
2016-01-30 17:44:54 -0500

    Deploy updates

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source "https://rubygems.org"
 
 group :development do
-  gem 'capistrano'
+  gem 'capistrano', '~> 2'
 end
 
 gem 'jekyll'
+gem 'rake', '~> 10.3.2'
--- config/deploy.rb
+++ config/deploy.rb
@@ -5,12 +5,15 @@ set :deploy_via, :remote_cache
 set :user, :deploy
 set :deploy_to, "/var/www/#{application}"
 set :use_sudo, false
+set :default_environment, {
+  'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
+}
 default_run_options[:pty] = true
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :blog, "176.58.105.165"
+role :blog, "personal"
 
 set :ssh_options, { :forward_agent => true }
 
@@ -22,6 +25,7 @@ set :ssh_options, { :forward_agent => true }
 
 namespace :jekyll do
   task :generate, :roles => :blog do
+    run "cd /var/www/#{application}/current && bundle install --without development"
     run "cd /var/www/#{application}/current && bundle exec jekyll build"
   end
 end
